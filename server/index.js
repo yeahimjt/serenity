149017d421975894d9a9e5e8d2d79dde32d8141f
yeahimjt <80553412+yeahimjt@users.noreply.github.com>
2023-07-24 16:43:27 -0500

    Updating cors

--- server/index.js
+++ server/index.js
@@ -22,8 +22,15 @@ app.use(cookieParser())
 
 app.use(bodyParser.json({ limit: "500mb", extended: true} ));
 app.use(bodyParser.urlencoded({ limit: "500mb", extended: true} ));
+// var corsOptions = {
+//     origin: 'http://localhost:3000',
+//     methods: ['GET', 'POST'],
+//     credentials: true,
+//     exposedHeaders: ["Set-Cookie"],
+
+// }
 var corsOptions = {
-    origin: 'http://localhost:3000',
+    origin: 'https://serenity-ddcv.onrender.com',
     methods: ['GET', 'POST'],
     credentials: true,
     exposedHeaders: ["Set-Cookie"],
@@ -34,10 +41,9 @@ app.use('/posts', postRoutes)
 app.use('/users', userRoutes)
 app.use('/messages', messageRoutes)
 
-const CONNECTION_URL = 'mongodb+srv://jonathanandrewtrevino:yB6UJxlgVCHcESV3@serenity.gf3rgbp.mongodb.net/?retryWrites=true&w=majority'
 const PORT = process.env.PORT || 5000;
 
-mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
     .then((error) => {if (error) throw error})
     .catch((err)=> console.log(err.message))
 
