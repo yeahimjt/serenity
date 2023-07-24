722a969d9192b5c3fd04f80d8bce18653440d016
yeahimjt <80553412+yeahimjt@users.noreply.github.com>
2023-07-24 17:33:49 -0500

    Updating api requests

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -12,17 +12,17 @@ export const updatePosts = (post_id, updateFields, image, status, tagsSelected) 
 export const postsById = (id) => axios.post(url + '/posts/id', {id})
 export const postsInspired = () => axios.get(url + '/posts/inspired', { withCredentials: true })
 export const postCategory = (filter) => axios.post(url + '/posts/category', {filter} ,{ withCredentials: true })
-export const getFeed = () => axios.post('/posts/feed', { withCredentials: true })
+export const getFeed = () => axios.post(url + '/posts/feed', { withCredentials: true })
 
 // ** NOTE ** For some reason not including the url in the axios requests solves issue with accessing cookie located in Application -> Cookies -> tokenn
 // Might need to tweak this in future if problem arises
 
 export const fetchUsers = () => axios.get(url + '/users', { withCredentials: true })
-export const createUsers = (newUser) => axios.post('/users', newUser, { withCredentials: true })
-export const loginUsers = (userData) => axios.post('/users/login', userData, { withCredentials: true })
-export const profileUsers = () => axios.post('/users/profile', { withCredentials: true })
-export const logout = () => axios.post('/users/logout', { withCredentials: true })
-export const updateUsers = (userData, images) => axios.post('/users/update', {userData,images}, { withCredentials: true })
+export const createUsers = (newUser) => axios.post(url + '/users', newUser, { withCredentials: true })
+export const loginUsers = (userData) => axios.post(url + '/users/login', userData, { withCredentials: true })
+export const profileUsers = () => axios.post(url + '/users/profile', { withCredentials: true })
+export const logout = () => axios.post(url + '/users/logout', { withCredentials: true })
+export const updateUsers = (userData, images) => axios.post(url + '/users/update', {userData,images}, { withCredentials: true })
 export const fetchUsersImage = (user_id) => axios.post(url + '/users/image', {user_id}, { withCredentials: true})
 export const getUserSecretly = (id) => axios.post(url + '/users/secret', {id}, {withCredentials: true})
 export const followUser = (id) => axios.post(url + '/users/follow', {id}, {withCredentials: true})
