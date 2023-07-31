18fe033f0b18d11263676fd52e3277453639754f
yeahimjt <80553412+yeahimjt@users.noreply.github.com>
2023-07-31 17:30:06 -0500

    Updating cors

--- server/index.js
+++ server/index.js
@@ -29,7 +29,7 @@ app.use(bodyParser.urlencoded({ limit: "500mb", extended: true} ));
 //     exposedHeaders: ["Set-Cookie"],
 // }
 var corsOptions = {
-    origin: ['www.serenity-peace-api.com'],
+    origin: ['https://www.serenity-peace-api.com/'],
     methods: ['GET', 'POST'],
     credentials: true,
 }
